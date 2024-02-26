a3ca10b734c72585e48236a7557b971de5eaffd2
Harshana-2000 <lakshara.21@cse.mrt.ac.lk>
2024-02-26 22:43:41 +0530

    create random color creater

--- build 25 react project/projects/src/App.jsx
+++ build 25 react project/projects/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 import Accoridian from './component/accordian';
-
+import RandomColor from './component/random-color';
+import Color from './component/color';
 function App() {
 
   return (
     <div className="App">
-      <Accoridian/>
+      {/* <Accoridian/>
+      <RandomColor/> */}
+      <Color/>
     </div>
   );
 }
